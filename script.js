bd592e6841fb1fa80a471e25a2b61a4dac90a058
Gilles Kinet <gilleskinet@gmail.com>
2026-03-20 08:58:09 +0100

    feat:  * add logic to remove all child nodes  * add logic to check for negative numbers or smaller than zero, add logic to check for text, add check for empty values

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let dimension = 16;
+let gridDimension = 16;
 
 const body = document.querySelector("body");
 const newGridButton = document.createElement("button");
@@ -7,21 +7,47 @@ body.insertBefore(newGridButton, container);
 newGridButton.textContent = "Click here to make a new grid";
 newGridButton.classList.add("new-grid-button");
 
-for (let i = 0; i < dimension; i++) {
-  const gridRow = document.createElement("div");
-  container.appendChild(gridRow);
-  gridRow.classList.add("grid-row");
+function createGrid(dimension = gridDimension) {
   for (let i = 0; i < dimension; i++) {
-    const gridItem = document.createElement("div");
-    gridRow.appendChild(gridItem);
-    gridItem.classList.add("grid-item");
+    const gridRow = document.createElement("div");
+    container.appendChild(gridRow);
+    gridRow.classList.add("grid-row");
+    for (let i = 0; i < dimension; i++) {
+      const gridItem = document.createElement("div");
+      gridRow.appendChild(gridItem);
+      gridItem.classList.add("grid-item");
+    }
+  }
+  const gridItems = document.querySelectorAll(".grid-item");
+
+  gridItems.forEach((item) => {
+    item.addEventListener("mouseover", (e) => {
+      e.target.classList.add("hover");
+    });
+  });
+}
+
+createGrid();
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
   }
 }
 
-const gridItems = document.querySelectorAll(".grid-item");
-
-gridItems.forEach((item) => {
-  item.addEventListener("mouseover", (e) => {
-    e.target.classList.add("hover");
-  });
+newGridButton.addEventListener("click", () => {
+  const promptText =
+    "How many squares do you want in your new grid? Max 100, min 1, use a number please";
+  gridDimension = prompt(promptText);
+  gridDimension = parseInt(gridDimension);
+  while (
+    gridDimension > 100 ||
+    gridDimension <= 0 ||
+    isNaN(gridDimension) ||
+    gridDimension === ""
+  ) {
+    gridDimension = prompt(promptText);
+  }
+  removeAllChildNodes(container);
+  createGrid(gridDimension);
 });
